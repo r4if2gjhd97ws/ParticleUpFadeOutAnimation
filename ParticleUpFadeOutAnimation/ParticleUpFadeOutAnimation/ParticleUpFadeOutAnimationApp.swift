1138a9ec5a962915cd8cec3d9f3c0eb76f3fef1d
yohei <>
2023-05-08 16:24:19 +0900

    初回upload

---
+++ ParticleUpFadeOutAnimation/ParticleUpFadeOutAnimation/ParticleUpFadeOutAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleUpFadeOutAnimationApp.swift
+//  ParticleUpFadeOutAnimation
+//
+//  Created by yohei on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct ParticleUpFadeOutAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
